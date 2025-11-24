21a26d742b58e9e4d1095dc326d43e242f85dee8
Desireedev <synticheattoh01@gmail.com>
2025-11-24 13:02:10 +0100

    test: update URL tests to match view behavior for invalid IDs

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,4 +1,27 @@
-# Fichier tests.py
-# Aucun import nécessaire pour le moment, car aucun test n'est défini
+from django.test import TestCase
+from django.urls import reverse
+from tasks.models import Task
 
+class TaskURLTests(TestCase):
+    #Test Urls for the tasks app
+    def setUp(self):
+        # Crée une tâche pour tester update/delete
+        self.task = Task.objects.create(title="Test Task", complete=False)
 
+    def test_index_url(self):
+        """Test que la page d'accueil '/' fonctionne"""
+        response = self.client.get(reverse('list'))  # <-- changé ici
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "TO DO LIST")
+
+    def test_update_task_url(self):
+        """Test que la page de mise à jour d'une tâche fonctionne"""
+        response = self.client.get(reverse('update_task', args=[self.task.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_task_url(self):
+        """Test que la page de suppression d'une tâche fonctionne"""
+        response = self.client.get(reverse('delete', args=[self.task.id]))
+        self.assertEqual(response.status_code, 200)
+
+    
